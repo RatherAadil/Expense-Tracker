5588e990f29870d5079255a245b75222561ff943
Rather Aadil <ratheraadil61@gmail.com>
2025-08-11 20:47:59 +0530

    Extract Form Data and display that in table

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import ExpenseForm from "../components/ExpenseForm";
 import ExpenseTable from "../components/ExpenseTable";
 import "./App.css";
+import ExpenseData from "./expenseData";
 
 function App() {
+  const [expenses, setExpenses] = useState(ExpenseData);
   return (
     <>
       <main>
         <h1>Track Your Expense</h1>
         <div className="expense-tracker">
-          <ExpenseForm />
-          <ExpenseTable />
+          <ExpenseForm setExpenses={setExpenses} />
+          <ExpenseTable expenses={expenses} />
         </div>
       </main>
     </>
