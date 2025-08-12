efabc9cbbbf2b12984c6c70db1943bdaf110a439
Rather Aadil <ratheraadil61@gmail.com>
2025-08-12 11:37:33 +0530

    Unidirectional Data flow/controlled input

--- components/ExpenseForm.jsx
+++ components/ExpenseForm.jsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ExpenseForm({ setExpenses }) {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    const expense = { ...getFormData(e.target), id: crypto.randomUUID() };
+    const expense = { title, category, amount, id: crypto.randomUUID() };
+
     setExpenses((prevState) => [...prevState, expense]);
-    e.target.reset();
-  };
-  const getFormData = (form) => {
-    const formData = new FormData(form);
-    const data = {};
-    for (const [key, value] of formData.entries()) {
-      data[key] = value;
-    }
-    return data;
+    setTitle("");
+    setCategory("");
+    setAmount("");
   };
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" />
+        <input
+          id="title"
+          name="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" name="category">
+        <select
+          id="category"
+          name="category"
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+        >
           <option hidden>Select Category</option>
           <option value="grocery">Grocery</option>
           <option value="clothes">Clothes</option>
@@ -34,7 +46,15 @@ function ExpenseForm({ setExpenses }) {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" />
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
       </div>
       <button className="add-btn">Add</button>
     </form>
